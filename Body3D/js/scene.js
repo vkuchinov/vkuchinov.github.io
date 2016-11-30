cef51caeb93c6dfe8a98d70260ab781ce34f6db8
whoof <helloworld@vkuchinov.co.uk>
2016-11-30 18:30:03 +0300

    update

--- Body3D/js/scene.js
+++ Body3D/js/scene.js
@@ -52,48 +52,6 @@ http://jsfiddle.net/shivasaxena/6gryajd6/3/
 			init();
 			animate();
 
-            function setHighPPI(canvas_) {
-    
-               var PIXEL_RATIO = (function () {
-                     var ctx = document.createElement("canvas").getContext("2d"),
-                         dpr = window.devicePixelRatio || 1,
-                         bsr = ctx.webkitBackingStorePixelRatio ||
-                         ctx.mozBackingStorePixelRatio ||
-                         ctx.msBackingStorePixelRatio ||
-                         ctx.oBackingStorePixelRatio ||
-                         ctx.backingStorePixelRatio || 1;
-
-                   return dpr / bsr; 
-
-               })();
-
-              canvas_.style.width  = canvas_.width  + "px";
-              canvas_.style.height = canvas_.height + "px";
-
-              canvas_.width  *= PIXEL_RATIO;
-              canvas_.height *= PIXEL_RATIO;
-
-              var context = canvas_.getContext('2d');
-              context.scale(PIXEL_RATIO, PIXEL_RATIO)
-
-            }
-
-            function getCoordinates(object_, camera_) {
-
-                var screenVector = new THREE.Vector3();
-                object_.localToWorld( screenVector );
-
-                screenVector.project(camera_);
-
-                var posx = Math.round(( screenVector.x + 1 ) * renderer.domElement.offsetWidth / 2 );
-                var posy = Math.round(( 1 - screenVector.y ) * renderer.domElement.offsetHeight / 2 );
-
-                return { x: posx, y: posy };
-            }
-
-            var hudBitmap, hudTexture, cameraHUD;
-
-
             $(document).ready(function() {
 
                 $.ajax({
@@ -236,53 +194,10 @@ http://jsfiddle.net/shivasaxena/6gryajd6/3/
 				renderer.setSize( window.innerWidth, window.innerHeight );
                 renderer.setClearColor(0x494949, 1.0);
 				container.appendChild( renderer.domElement );
-		
-				
-                var hudCanvas = document.createElement('canvas');
-  
-                hudCanvas.width =  window.innerWidth;
-                hudCanvas.height =  window.innerHeight;
-                
-                // Get 2D context and draw something supercool.
-                hudBitmap = hudCanvas.getContext('2d');
-                
-                setHighPPI(hudCanvas);
-
-                hudBitmap.beginPath();
-                hudBitmap.arc(0, 0, 16, 0, 2 * Math.PI, false);
-                //hudBitmap.lineWidth = 4;
-                //hudBitmap.strokeStyle = '#FFFFFF';
-                //hudBitmap.stroke();
-                hudBitmap.fillStyle = '#FFFFFF';
-                hudBitmap.fill();
-                hudBitmap.font = "Normal 14px Arial";
-                hudBitmap.textAlign = 'left';
-                hudBitmap.fillStyle = "rgba(255, 255, 255, 0.75)";
-                //hudBitmap.fillText('',  0, 0);
-  
-                // Create the camera and set the viewport to match the screen dimensions.
-                cameraHUD = new THREE.OrthographicCamera(- window.innerWidth/2,  window.innerWidth/2,  window.innerHeight/2, - window.innerHeight/2, 0, 30 );
-
-                // Create also a custom scene for HUD.
-                sceneHUD = new THREE.Scene();
- 
-                // Create texture from rendered graphics.
-                hudTexture = new THREE.Texture(hudCanvas) 
-                hudTexture.needsUpdate = true;
-                hudTexture.minFilter = THREE.LinearFilter
-  
-  // Create HUD material.
-  var material = new THREE.MeshBasicMaterial( {map: hudTexture} );
-  material.transparent = true;
-
-  // Create plane to render the HUD. This plane fill the whole screen.
-  var planeGeometry = new THREE.PlaneGeometry(  window.innerWidth,  window.innerHeight );
-  var plane = new THREE.Mesh( planeGeometry, material );
-  sceneHUD.add( plane );
-                
+				document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+				//
 				window.addEventListener( 'resize', onWindowResize, false );
                 document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-                
     
 			}
 
@@ -342,38 +257,6 @@ http://jsfiddle.net/shivasaxena/6gryajd6/3/
 				}
 
                 }
-                
-                 var object = scene.getObjectByName( "body" );
-                 var xy = getCoordinates(object, camera);
-
-                
-                               
-                 var hor = 1;
-                 var ver = 1;
-                
-                 if(xy.x < window.innerWidth / 2) hor = -1;
-                 if(xy.y < window.innerHeight / 2) ver = -1;
-                 //console.log(window.innerWidth);
-                 hudBitmap.clearRect(0, 0,  window.innerWidth,  window.innerHeight);
-                
-                 hudBitmap.beginPath();
-                 hudBitmap.arc(xy.x, xy.y, 8, 0, 2 * Math.PI, false);
-                 hudBitmap.fillStyle = '#FFFFFF';
-                 hudBitmap.fill();
-  
-                 hudBitmap.strokeStyle = '#FFFFFF';
-                 hudBitmap.moveTo(xy.x, xy.y);
-                 hudBitmap.lineTo(xy.x + 32  * hor, xy.y - 32 * ver);
-                 hudBitmap.lineTo(xy.x + 192 * hor, xy.y - 32  * ver);
-                 hudBitmap.lineWidth = 2;
-                 hudBitmap.stroke();
-                
-                 //hudBitmap.lineWidth = 5;
-                 //hudBitmap.strokeStyle = '#FFFFFF49';
-                 //hudBitmap.stroke();
-                 if(hor == 1 ) { hudBitmap.textAlign = 'right'; } else { hudBitmap.textAlign = 'left'; };
-                 hudBitmap.fillText("Easy-to-read display", xy.x + 192 * hor, xy.y - 32 * ver - 6);
-  	             hudTexture.needsUpdate = true;
                 
 				renderer.render( scene, camera );
 			}
